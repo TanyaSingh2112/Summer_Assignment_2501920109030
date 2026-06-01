f83e98e38ac1e316535c7014193801687a8d3609
TanyaSingh2112 <ts090045@gmail.com>
2026-06-01 20:30:01 +0530

    Add files via upload

---
+++ Week_1/Day_1/TwoSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int [] result = new int[2];
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for(int i=0; i<nums.length; i++){
+        int desire = target - nums[i];
+        if(map.containsKey(desire)){
+            result[0] = map.get(desire);
+            result[1] = i;
+        }
+        map.put(nums[i], i);
+       }
+       return result;
+    }
+}
